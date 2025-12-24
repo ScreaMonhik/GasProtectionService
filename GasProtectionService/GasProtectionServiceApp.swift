15e1333c07d393c4c46f53e5114df7e369726b61
Sunko Dmytro <virag5002@gmail.com>
2025-12-24 09:46:36 +0200

    Initial Commit

---
+++ GasProtectionService/GasProtectionServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasProtectionServiceApp.swift
+//  GasProtectionService
+//
+//  Created by Dima Sunko on 24.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct GasProtectionServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
